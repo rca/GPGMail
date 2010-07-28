9eb296989be1e8d4ab4c6e492054b2d3eb503bdd
Alexander Willner <alex@willner.ws>
2010-07-28 16:25:23 +0200

    We're now on version 1.3.1-beta1

--- GPGMail/create_zipfile.sh
+++ GPGMail/create_zipfile.sh
@@ -2,4 +2,4 @@
 
 # cd to dir and then make zipfile
 cd build/Release
-ditto -ck --keepParent "GPGMail.mailbundle" "GPGMail.zip"
+ditto -ck --keepParent "GPGMail.mailbundle" "GPGMail-1.3.1-beta1.zip"
