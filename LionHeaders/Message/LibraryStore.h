a4d3a25e5f2f8b3a08d27ec8ff6070826148aab5
Lukas Pitschl <lukas@dressyvagabonds.com>
2011-08-28 01:04:00 +0200

    Might be used for generating snippets. Works already like a charm at the moment, but this way it can be forced.

--- LionHeaders/Message/LibraryStore.h
+++ LionHeaders/Message/LibraryStore.h
@@ -4,7 +4,7 @@
  *     class-dump is Copyright (C) 1997-1998, 2000-2001, 2004-2010 by Steve Nygard.
  */
 
-#import <Message/MessageStore.h>
+#import <MessageStore.h>
 
 @class MessageCriterion, NSMutableArray, NSMutableSet, NSString;
 
