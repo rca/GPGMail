849b003b632f4c32cba898a791c8698a79f9a073
Lukas Pitschl <lukas@dressyvagabonds.com>
2012-03-01 06:25:35 +0100

    Add a debug log message for the default security method

--- Source/GMSecurityHistory.m
+++ Source/GMSecurityHistory.m
@@ -35,8 +35,10 @@
 
 + (GPGMAIL_SECURITY_METHOD)defaultSecurityMethod {
     GPGMAIL_SECURITY_METHOD securityMethod = GPGMAIL_SECURITY_METHOD_OPENPGP;
-    if([[GPGOptions sharedOptions] integerForKey:@"DefaultSecurityMethod"])
+    if([[GPGOptions sharedOptions] integerForKey:@"DefaultSecurityMethod"]) {
         securityMethod = [[GPGOptions sharedOptions] integerForKey:@"DefaultSecurityMethod"];
+        DebugLog(@"Default Security Method is: %@", securityMethod != 0 && securityMethod == GPGMAIL_SECURITY_METHOD_SMIME ? @"S/MIME" : @"OpenPGP");
+    }
     return securityMethod;
 }
 
