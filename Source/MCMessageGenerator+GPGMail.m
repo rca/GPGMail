de0c64f9e1716568a86d71abb062266380e0f1e6
Lukas Pitschl <lukas@dressyvagabonds.com>
2014-11-29 02:28:11 +0100

    [FIX] Encrypted drafts of multipart messages are no longer being mangled on save ([#782 state:fixed])
    - The mangled drafts had too many headers stripped in an attempt to workaround a MailTags bug.
      When MailTags 4.0.2 was installed, the first part of each message was duplicated and thus
      created mangled encrypted drafts. In an overzealous attempt to fix this issue, GPGMail stripped
      out the first part if it detected two parts, not considering that it's necessary to check that
      the two first parts are identical otherwise it would affect various multipart messages.

--- Source/MCMessageGenerator+GPGMail.m
+++ Source/MCMessageGenerator+GPGMail.m
@@ -37,12 +37,19 @@
 - (id)MA_newDataForMimePart:(id)mimePart withPartData:(id)partData NS_RETURNS_RETAINED {
 	// MailTags likes to duplicate the headers of the mime part for some reason.
 	// It looks like it's easily detectable by checking for the header and body separator \n\n.
-	// If we find two header separators, we can assume that a duplicate header was added
-	// and will remove the first one.
+	// If we find two header separators and the two headers look exactly the same,
+	// we can assume that a duplicate header was added and will remove the first one.
+	//
+	// It's crucial that the first two part headers are identical, otherwise we're simply dealing
+	// with a normal multipart message and would remove too many headers and thus mangle the original
+	// message.
 	
+	NSData *newData = [self MA_newDataForMimePart:mimePart withPartData:partData];
+	// If MailTags is not installed, let's get out of here.
+	if(NSClassFromString(@"MailTagsBundle") == nil)
+		return newData;
 	// The bug has only been seen in combinations with encryted drafts yet, so in any other case,
 	// we'll just call into the original method and return the result.
-	NSData *newData = [self MA_newDataForMimePart:mimePart withPartData:partData];
 	if([mimePart parentPart] || ![[self getIvar:@"IsDraft"] boolValue] || !(BOOL)[(GM_CAST_CLASS(MCMessageGenerator *, id))self encryptsOutput])
 		return newData;
 	
@@ -51,17 +58,23 @@
 	NSData *NL = [@"\n\n" dataUsingEncoding:NSUTF8StringEncoding];
 	
 	NSUInteger messageSeparatorsFound = 0;
+	NSData *firstHeader, *secondHeader = nil;
 	NSRange firstHeaderEndRange = [newData rangeOfData:NL options:0 range:NSMakeRange(0, [newData length])];
 	// One header separator is found. Expected, so on to the next check.
 	if(firstHeaderEndRange.location != NSNotFound) {
+		// The first header is found between location 0 and the first occurence of \n\n which is firstHeaderEndRange.location
+		firstHeader = [newData subdataWithRange:NSMakeRange(0, firstHeaderEndRange.location)];
 		messageSeparatorsFound++;
 		NSRange secondHeaderEndRange = [newData rangeOfData:NL options:0 range:NSMakeRange(firstHeaderEndRange.location + firstHeaderEndRange.length, [newData length] - firstHeaderEndRange.location - firstHeaderEndRange.length)];
 		// Oh oh, a second one has been found.
-		if(secondHeaderEndRange.location != NSNotFound)
+		if(secondHeaderEndRange.location != NSNotFound) {
+			// The second header is found between firstHeaderEndRange.location + firstHeaderEndRange.length and spreads until secondHeaderEndRange.location.
+			secondHeader = [newData subdataWithRange:NSMakeRange(firstHeaderEndRange.location + firstHeaderEndRange.length, secondHeaderEndRange.location - (firstHeaderEndRange.location + firstHeaderEndRange.length))];
 			messageSeparatorsFound++;
+		}
 	}
-	// Two header separators? Let's only keep the data after the first one.
-	if(messageSeparatorsFound > 1)
+	// Two header separators and the headers are equal? Let's only keep the data after the first one.
+	if(messageSeparatorsFound > 1 && [firstHeader isEqualToData:secondHeader])
 		newData = [newData subdataWithRange:NSMakeRange(firstHeaderEndRange.location + firstHeaderEndRange.length, [newData length] - firstHeaderEndRange.location - firstHeaderEndRange.length)];
 	return newData;
 }
